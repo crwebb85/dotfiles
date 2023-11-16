3884df5c6d52aadbf8e398856890585f12af235d
crwebb85 <crwebb85@gmail.com>
2023-11-16 03:14:15 +0000

    Added trouble.nvim plugin for managing diagnostics

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -165,10 +165,44 @@ require('lazy').setup({
         end,
         opts = {},
     },
+    -- Open terminal within neovim
     {
         'akinsho/toggleterm.nvim',
         version = '*',
         config = true,
+    },
+    -- Diagnostic info
+    {
+        'folke/trouble.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        keys = {
+            { '<leader>xx', function() require('trouble').toggle() end },
+            {
+                '<leader>xw',
+                function() require('trouble').toggle('workspace_diagnostics') end,
+            },
+            {
+                '<leader>xd',
+                function() require('trouble').toggle('document_diagnostics') end,
+            },
+            {
+                '<leader>xq',
+                function() require('trouble').toggle('quickfix') end,
+            },
+            {
+                '<leader>xl',
+                function() require('trouble').toggle('loclist') end,
+            },
+            {
+                'gR',
+                function() require('trouble').toggle('lsp_references') end,
+            },
+        },
     },
     -- Adds motions to wrap text in qoutes/brackets/tags/etc
     -- using the same motions I use to yank text
