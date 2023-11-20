0b3bfbae15ac9b9cd643344665107a82e9f8de67
crwebb85 <crwebb85@gmail.com>
2023-11-20 20:42:04 +0000

    Add more keymaps for debugger

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -594,17 +594,77 @@ require('lazy').setup({
             {
                 '<leader>dc',
                 function() require('dap').continue() end,
-                desc = 'Start/Continue Debugger',
+                desc = 'Debug: Start/Continue Debugger',
             },
             {
                 '<leader>db',
                 function() require('dap').toggle_breakpoint() end,
-                desc = 'Add Breakpoint',
+                desc = 'Debug: Add Breakpoint',
             },
             {
                 '<leader>dt',
                 function() require('dap').terminate() end,
-                desc = 'Terminate Debugger',
+                desc = 'Debug: Terminate Debugger',
+            },
+            {
+                '<leader>dC',
+                function() require('dap').run_to_cursor() end,
+                desc = 'Debug: Run to Cursor',
+            },
+            {
+                '<leader>dg',
+                function() require('dap').goto_() end,
+                desc = 'Debug: Go to line (no execute)',
+            },
+            {
+                '<leader>di',
+                function() require('dap').step_into() end,
+                desc = 'Debug: Step Into',
+            },
+            {
+                '<leader>dj',
+                function() require('dap').down() end,
+                desc = 'Down',
+            },
+            {
+                '<leader>dk',
+                function() require('dap').up() end,
+                desc = 'Debug: Up',
+            },
+            {
+                '<leader>dl',
+                function() require('dap').run_last() end,
+                desc = 'Debug: Run Last',
+            },
+            {
+                '<leader>do',
+                function() require('dap').step_out() end,
+                desc = 'Debug: Step Out',
+            },
+            {
+                '<leader>dO',
+                function() require('dap').step_over() end,
+                desc = 'Debug: Step Over',
+            },
+            {
+                '<leader>dp',
+                function() require('dap').pause() end,
+                desc = 'Debug: Pause',
+            },
+            {
+                '<leader>dr',
+                function() require('dap').repl.toggle() end,
+                desc = 'Debug: Toggle REPL',
+            },
+            {
+                '<leader>ds',
+                function() require('dap').session() end,
+                desc = 'Debug: Session',
+            },
+            {
+                '<leader>dw',
+                function() require('dap.ui.widgets').hover() end,
+                desc = 'Debug: Widgets',
             },
         },
     },
@@ -621,7 +681,7 @@ require('lazy').setup({
             {
                 '<leader>du',
                 function() require('dapui').toggle() end,
-                desc = 'Toggle Debugger UI',
+                desc = 'Debug: Toggle Debugger UI',
             },
         },
         -- automatically open/close the DAP UI when starting/stopping the debugger
