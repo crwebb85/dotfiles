7f2bc10e91674427f662a22a9ba9cd8b298d06e1
crwebb85 <crwebb85@gmail.com>
2023-12-26 13:47:28 +0000

    Updated undo dir path to use platform seperator

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -15,7 +15,8 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.undodir = os.getenv('HOME') .. '/.vim/undodir'
+vim.undodir =
+    require('utils.path').concat({ os.getenv('HOME'), '.vim', 'undodir' })
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
