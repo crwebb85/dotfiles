df6fe442f713e2b308945af875ce7333b51576ab
Chris <crewebb85@gmail.com>
2024-01-06 11:28:45 -0500

    Fix toggleterm config so that it runs default settings when not on Windows platform

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -575,18 +575,20 @@ require('lazy').setup({
         'akinsho/toggleterm.nvim',
         lazy = true,
         config = function(_, opts)
-            local powershell_options = {
-                shell = vim.fn.executable('pwsh') == 1 and 'pwsh'
-                    or 'powershell',
-                shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::Default;',
-                shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
-                shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
-                shellquote = '',
-                shellxquote = '',
-            }
+            if require('utils.platform').is.win then
+                local powershell_options = {
+                    shell = vim.fn.executable('pwsh') == 1 and 'pwsh'
+                        or 'powershell',
+                    shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::Default;',
+                    shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
+                    shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
+                    shellquote = '',
+                    shellxquote = '',
+                }
 
-            for option, value in pairs(powershell_options) do
-                vim.opt[option] = value
+                for option, value in pairs(powershell_options) do
+                    vim.opt[option] = value
+                end
             end
             require('toggleterm').setup(opts)
         end,
