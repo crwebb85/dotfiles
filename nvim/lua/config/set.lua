4b4386a3379821d391b0e1c46fa362a4fe756411
Chris <crwebb85@gmail.com>
2024-05-13 17:14:24 -0400

    Added preview window for search and replace

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -62,6 +62,8 @@ vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.opt.pumheight = 15 -- limits the size of the completion menu to only show 15 items at a time
 vim.opt.pumblend = 15 -- makes the completion menu slightly transparent
 
+vim.opt.inccommand = 'split' -- shows search in replace changes in a preview window
+
 vim.filetype.add({
     extension = {
         json = 'jsonc', -- This ensures that I can comment lines in json configuration files even if the extension is .json
