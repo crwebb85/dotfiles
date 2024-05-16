585217b93d8a1964462ce31f704b88c8c3d83879
Chris <crwebb85@gmail.com>
2024-05-15 21:41:39 -0400

    Organized options

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -1,14 +1,34 @@
+-------------------------------------------------------------------------------
+--- Look and feel
 vim.opt.title = true
 vim.opt.titlestring = [[%t – %{fnamemodify(getcwd(), ':t')}]]
-
+vim.opt.colorcolumn = '80'
 vim.opt.relativenumber = true
 vim.opt.number = true
+vim.opt.wrap = false
 
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = 'yes'
+vim.opt.isfname:append('@-@')
+
+-------------------------------------------------------------------------------
+--- Performance
+vim.opt.lazyredraw = true -- redraw only when required (will lazily redraw during macros)
+vim.opt.updatetime = 50
+
+-------------------------------------------------------------------------------
+--- Tabs
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.smartindent = true
 
+-------------------------------------------------------------------------------
+--- Spell
+vim.opt.spell = true
+vim.opt.spelllang = 'en_us'
+-- vim.g.spellfile_URL = 'https://ftp.nluug.nl/vim/runtime/spell'
 -- local function get_spellfile_path()
 --     local path_utils = require('utils.path')
 --     local data_path = vim.fn.stdpath('config')
@@ -26,53 +46,50 @@ vim.opt.expandtab = true
 --     return path
 -- end
 -- get_spellfile_path()
-
--- vim.g.spellfile_URL = 'https://ftp.nluug.nl/vim/runtime/spell'
-vim.opt.spell = true
-vim.opt.spelllang = 'en_us'
 -- vim.opt.spellfile = get_spellfile_path()
-
 -- vim.cmd([[set spellfile="]] .. get_spellfile_path() .. [["]])
 
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-
+-------------------------------------------------------------------------------
+--- Disable/Enable backups
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = 'yes'
-vim.opt.isfname:append('@-@')
-
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = '80'
-
+-------------------------------------------------------------------------------
+--- Folding
 vim.opt.foldcolumn = '1'
 vim.opt.foldmethod = 'expr'
+-- TODO fallback to different folding expr when treesitter folding is not available
 vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 
+-------------------------------------------------------------------------------
+--- Completion menu
 vim.opt.pumheight = 15 -- limits the size of the completion menu to only show 15 items at a time
 vim.opt.pumblend = 15 -- makes the completion menu slightly transparent
 
+-------------------------------------------------------------------------------
+--- Search and replace
 vim.opt.inccommand = 'split' -- shows search in replace changes in a preview window
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
+-------------------------------------------------------------------------------
+--- Filetype overrides
 vim.filetype.add({
     extension = {
         json = 'jsonc', -- This ensures that I can comment lines in json configuration files even if the extension is .json
     },
 })
 
+-------------------------------------------------------------------------------
+--- Error formats
 -- Error format for nuget restore:
 -- helloworld\helloworld.csproj : warning NU1901: Package 'my.helloworld' 1.0.0 has a known low severity vulnerability
 vim.cmd([[ set errorformat+=%f:\ %tarning\ %m ]])
+
+-------------------------------------------------------------------------------
+--- Terminal
+vim.opt.termguicolors = true
 
 if require('utils.platform').is.win then
     vim.opt.shell = vim.fn.executable('pwsh') == 1 and 'pwsh' or 'powershell'
