e706e57ee0f501e3ad3facfa8e63487fe19a9745
Chris <crwebb85@gmail.com>
2024-05-28 08:51:18 -0400

    Fixed inlay hints with new api function signature

--- nvim/lua/config/lsp/inlayhints.lua
+++ nvim/lua/config/lsp/inlayhints.lua
@@ -9,12 +9,14 @@ function M.toggle_inlay_hints()
 
     isInlayHintsEnabled = not isInlayHintsEnabled
 
-    vim.lsp.inlay_hint.enable(0, isInlayHintsEnabled)
+    vim.lsp.inlay_hint.enable(isInlayHintsEnabled, { bufnr = 0 })
 
     vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
         group = vim.api.nvim_create_augroup('inlay_hints', { clear = true }),
         pattern = '?*',
-        callback = function() vim.lsp.inlay_hint.enable(0, isInlayHintsEnabled) end,
+        callback = function()
+            vim.lsp.inlay_hint.enable(isInlayHintsEnabled, { bufnr = 0 })
+        end,
     })
 end
 
