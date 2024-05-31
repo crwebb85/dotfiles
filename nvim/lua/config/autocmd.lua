f78571119933b1670aedc14438ec341ab67eae29
Chris <crwebb85@gmail.com>
2024-05-31 08:00:04 -0400

    Added autosave snippet to try out later

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -167,3 +167,16 @@ vim.api.nvim_create_autocmd({ 'VimResized' }, {
     group = vim.api.nvim_create_augroup('resize_splits', { clear = true }),
     callback = function() vim.cmd('tabdo wincmd =') end,
 })
+
+-------------------------------------------------------------------------------
+--- Autosave
+-- vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'VimLeavePre' }, {
+--     pattern = '*',
+--     group = vim.api.nvim_create_augroup('autosave', { clear = true }),
+--     callback = function(event)
+--         if event.buftype or event.file == '' then return end
+--         vim.api.nvim_buf_call(event.buf, function()
+--             vim.schedule(function() vim.cmd('silent! write') end)
+--         end)
+--     end,
+-- })
