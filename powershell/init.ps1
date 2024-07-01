d568006af29c08e1258ac556ef84327e37f52a16
Chris <crwebb85@gmail.com>
2024-07-01 12:18:19 -0400

    Added git branch name to powershell prompt

--- powershell/init.ps1
+++ powershell/init.ps1
@@ -1,12 +1,50 @@
-$checkhealthConfigPath = Get-ChildItem $PSScriptRoot\init\checkhealth.ps1 | Select -ExpandProperty FullName
-echo "Sourcing $checkhealthConfigPath"
+$checkhealthConfigPath = Get-ChildItem $PSScriptRoot\init\checkhealth.ps1 | Select-Object -ExpandProperty FullName
+Write-Output "Sourcing $checkhealthConfigPath"
 . "$checkhealthConfigPath"
 
-$readlineConfigPath = Get-ChildItem $PSScriptRoot\init\PSFzf\PSFzf.ps1 | Select -ExpandProperty FullName
-echo "Sourcing $readlineConfigPath"
+$readlineConfigPath = Get-ChildItem $PSScriptRoot\init\PSFzf\PSFzf.ps1 | Select-Object -ExpandProperty FullName
+Write-Output "Sourcing $readlineConfigPath"
 . "$readlineConfigPath"
 
-$readlineConfigPath = Get-ChildItem $PSScriptRoot\init\readline.ps1 | Select -ExpandProperty FullName
-echo "Sourcing $readlineConfigPath"
+$readlineConfigPath = Get-ChildItem $PSScriptRoot\init\readline.ps1 | Select-Object -ExpandProperty FullName
+Write-Output "Sourcing $readlineConfigPath"
 . "$readlineConfigPath"
 
+
+function Write-BranchName () {
+    try {
+        $branch = git rev-parse --abbrev-ref HEAD
+
+        if ($branch -eq "HEAD") {
+            # we're probably in detached HEAD state, so print the SHA
+            $branch = git rev-parse --short HEAD
+            Write-Host " ($branch)" -ForegroundColor "red"
+        } else {
+            # we're on an actual branch, so print it
+            Write-Host " ($branch)" -ForegroundColor "blue"
+        }
+    } catch {
+        # we'll end up here if we're in a newly initiated git repo
+        Write-Host " (no branches yet)" -ForegroundColor "yellow"
+    }
+}
+
+# Sets the look of the prompt line specifically adding the git branch name
+function prompt {
+    # From https://stackoverflow.com/a/44411205
+    $base = "PS "
+    $path = "$($executionContext.SessionState.Path.CurrentLocation)"
+    $userPrompt = "$('>' * ($nestedPromptLevel + 1)) "
+
+    Write-Host "`n$base" -NoNewline
+
+    if (Test-Path .git) {
+        Write-Host $path -NoNewline -ForegroundColor "green"
+        Write-BranchName
+    } else {
+        # we're not in a repo so don't bother displaying branch name/sha
+        Write-Host $path -ForegroundColor "green"
+    }
+
+    return $userPrompt
+}
