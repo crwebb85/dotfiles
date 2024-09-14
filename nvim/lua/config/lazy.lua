3017cb3211366a98b1e32d924ced0041ffcda4cc
Chris <crwebb85@gmail.com>
2024-09-14 09:48:40 -0400

    reorganized tressiter plugins

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1111,6 +1111,85 @@ require('lazy').setup({
         config = true,
     },
 
+    ---------------------------------------------------------------------------
+    -- Treesitter Parsers and Parser Utils
+    {
+        'nvim-treesitter/nvim-treesitter',
+        lazy = true,
+        event = 'VeryLazy',
+        build = ':TSUpdate',
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                modules = {},
+                -- A list of parser names, or "all"
+                ensure_installed = {
+                    'diff',
+                    'javascript',
+                    'typescript',
+                    'tsx',
+                    'css',
+                    'json',
+                    'jsonc',
+                    'html',
+                    'xml',
+                    'yaml',
+                    'c',
+                    'lua',
+                    'rust',
+                    'vim',
+                    'vimdoc',
+                    'query',
+                    'markdown',
+                    'markdown_inline',
+                    'python',
+                    'toml',
+                    'regex',
+                    'c_sharp',
+                },
+
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
+
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = true,
+
+                -- List of parsers to ignore installing (or "all")
+                ignore_install = {},
+
+                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+                highlight = {
+                    enable = true,
+
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                },
+            })
+            local query = [[
+                (cast_expression
+                  (
+                    ("(") @start 
+                    .
+                    type: (predefined_type)  @cast.inner
+                    .
+                    ")" @end
+                    (#make-range! "cast.outer" @start @end)
+                    )
+                  ) 
+            ]]
+            vim.treesitter.query.set(
+                'c_sharp',
+                config.MY_CUSTOM_TREESITTER_TEXTOBJECT_GROUP,
+                query
+            )
+        end,
+    },
+
     --tressiter split and join nodes
     {
         'Wansmer/treesj',
@@ -1344,85 +1423,6 @@ require('lazy').setup({
             'nvim-treesitter/nvim-treesitter',
         },
         cmd = { 'Refactor' },
-    },
-
-    ---------------------------------------------------------------------------
-    -- Parser for syntax highlighting
-    {
-        'nvim-treesitter/nvim-treesitter',
-        lazy = true,
-        event = 'VeryLazy',
-        build = ':TSUpdate',
-        config = function()
-            require('nvim-treesitter.configs').setup({
-                modules = {},
-                -- A list of parser names, or "all"
-                ensure_installed = {
-                    'diff',
-                    'javascript',
-                    'typescript',
-                    'tsx',
-                    'css',
-                    'json',
-                    'jsonc',
-                    'html',
-                    'xml',
-                    'yaml',
-                    'c',
-                    'lua',
-                    'rust',
-                    'vim',
-                    'vimdoc',
-                    'query',
-                    'markdown',
-                    'markdown_inline',
-                    'python',
-                    'toml',
-                    'regex',
-                    'c_sharp',
-                },
-
-                -- Install parsers synchronously (only applied to `ensure_installed`)
-                sync_install = false,
-
-                -- Automatically install missing parsers when entering buffer
-                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-                auto_install = true,
-
-                -- List of parsers to ignore installing (or "all")
-                ignore_install = {},
-
-                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-                highlight = {
-                    enable = true,
-
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
-                    additional_vim_regex_highlighting = false,
-                },
-            })
-            local query = [[
-                (cast_expression
-                  (
-                    ("(") @start 
-                    .
-                    type: (predefined_type)  @cast.inner
-                    .
-                    ")" @end
-                    (#make-range! "cast.outer" @start @end)
-                    )
-                  ) 
-            ]]
-            vim.treesitter.query.set(
-                'c_sharp',
-                config.MY_CUSTOM_TREESITTER_TEXTOBJECT_GROUP,
-                query
-            )
-        end,
     },
 
     ---------------------------------------------------------------------------
