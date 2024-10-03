ec9d52875566a5ab3ece4e29ed70dd08d7a4850b
Chris <crwebb85@gmail.com>
2024-10-03 09:07:32 -0400

    Added telescope picker for my notes folder

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -549,6 +549,23 @@ require('lazy').setup({
                 end,
                 desc = 'Telescope: harpoon',
             },
+
+            {
+                '<leader>fn',
+                function()
+                    local note_path = vim.fs.normalize('$MY_NOTES')
+                    if note_path == '$MY_NOTES' then
+                        error(
+                            "$MY_NOTES environment variable is not defined. Please create the environment variable in order to search it's directory."
+                        )
+                    end
+                    require('telescope.builtin').find_files({
+                        cwd = note_path,
+                    })
+                end,
+                desc = 'Telescope: find files in note directory ',
+                mode = { 'n' },
+            },
         },
         opts = {
             defaults = {
