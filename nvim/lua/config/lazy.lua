1b2d4877a5432825778194cf411e27ec06c77c7d
Chris <crwebb85@gmail.com>
2025-03-22 12:55:29 -0400

    Added treesitter queries

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1722,10 +1722,50 @@ require('lazy').setup({
                 (constructor_declaration
                     name: (_) @function_declaration_name.inner)
             ]]
+            local custom_lua_queries = [[
+              ; function M.is_dict() ... end
+                  (function_declaration 
+                    name: (dot_index_expression 
+                      field: (_) @function_declaration_name.inner
+                    )
+                  ) 
+
+              ;local is_dict = function() ... end
+              local_declaration: (variable_declaration 
+                (assignment_statement 
+                  (variable_list 
+                    name: (identifier) @function_declaration_name.inner ) 
+                  (expression_list 
+                    value: (function_definition))
+                )
+              )
+
+              ; M.is_dict = function() ... end
+              (assignment_statement 
+                (variable_list 
+                  name: (dot_index_expression 
+                      field: (_) @function_declaration_name.inner))
+                (expression_list 
+                  value: (function_definition))
+                )
+
+              ; local function is_dict() ... end
+              local_declaration: (function_declaration 
+                name: (identifier) @function_declaration_name.inner
+              ) 
+
+
+            ]]
             vim.treesitter.query.set(
                 'c_sharp',
                 config.MY_CUSTOM_TREESITTER_TEXTOBJECT_GROUP,
                 custom_csharp_queries
+            )
+
+            vim.treesitter.query.set(
+                'lua',
+                config.MY_CUSTOM_TREESITTER_TEXTOBJECT_GROUP,
+                custom_lua_queries
             )
         end,
     },
