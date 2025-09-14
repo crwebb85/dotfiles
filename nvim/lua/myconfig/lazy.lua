4a6bcd63111f46d08e9cd184760960ceaaf8547d
Chris <crwebb85@gmail.com>
2025-09-14 07:50:18 -0400

    feat(plugin) added nvim-treesitter-context

--- nvim/lua/myconfig/lazy.lua
+++ nvim/lua/myconfig/lazy.lua
@@ -1695,6 +1695,27 @@ require('lazy').setup({
             )
         end,
     },
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        config = function(_, _)
+            require('treesitter-context').setup({
+                enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+                multiwindow = false, -- Enable multiwindow support.
+                max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+                min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+                line_numbers = true,
+                multiline_threshold = 20, -- Maximum number of lines to show for a single context
+                trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+                mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+                -- Separator between context and content. Should be a single character string, like '-'.
+                -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+                separator = nil,
+                zindex = 20, -- The Z-index of the context window
+                on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+            })
+        end,
+    },
 
     --tressiter split and join nodes
     {
@@ -1721,7 +1742,7 @@ require('lazy').setup({
             },
         },
         dependencies = { 'nvim-treesitter/nvim-treesitter' },
-        config = function(_)
+        config = function(_, _)
             local lang_utils = require('treesj.langs.utils')
             local opts = {
                 use_default_keymaps = false,
