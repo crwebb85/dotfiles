c9e327be6278568f58eaf0dd95486bddc2220c1c
Chris <crwebb85@gmail.com>
2025-12-15 14:03:22 -0500

    refactor(plugins): Moved snippet plugin config back to lazy.lua

--- nvim/lua/myconfig/lazy.lua
+++ nvim/lua/myconfig/lazy.lua
@@ -2497,6 +2497,64 @@ require('lazy').setup({
     },
 
     {
+        'L3MON4D3/LuaSnip',
+        lazy = true,
+        dependencies = {
+            'rafamadriz/friendly-snippets',
+            {
+                'benfowler/telescope-luasnip.nvim',
+                dependencies = {
+                    'nvim-telescope/telescope.nvim',
+                },
+                config = function()
+                    require('telescope').load_extension('luasnip')
+                end,
+            },
+        },
+        config = function(_, opts)
+            if opts then require('luasnip').config.setup(opts) end
+            vim.tbl_map(
+                function(type)
+                    require('luasnip.loaders.from_' .. type).lazy_load()
+                end,
+                { 'vscode', 'snipmate', 'lua' }
+            )
+            local config_path = vim.fn.stdpath('config')
+            if config_path ~= nil and type(config_path) == 'string' then
+                local luasnip_path = vim.fs.joinpath(config_path, 'LuaSnip/')
+                require('luasnip.loaders.from_lua').load({
+                    paths = {
+                        luasnip_path,
+                    },
+                })
+            end
+            -- friendly-snippets - enable standardized comments snippets
+            require('luasnip').filetype_extend('typescript', { 'tsdoc' })
+            require('luasnip').filetype_extend('javascript', { 'jsdoc' })
+            require('luasnip').filetype_extend('lua', { 'luadoc' })
+            require('luasnip').filetype_extend('python', { 'pydoc' })
+            require('luasnip').filetype_extend('rust', { 'rustdoc' })
+            require('luasnip').filetype_extend('cs', { 'csharpdoc' })
+            require('luasnip').filetype_extend('java', { 'javadoc' })
+            require('luasnip').filetype_extend('c', { 'cdoc' })
+            require('luasnip').filetype_extend('cpp', { 'cppdoc' })
+            require('luasnip').filetype_extend('php', { 'phpdoc' })
+            require('luasnip').filetype_extend('kotlin', { 'kdoc' })
+            require('luasnip').filetype_extend('ruby', { 'rdoc' })
+            require('luasnip').filetype_extend('sh', { 'shelldoc' })
+        end,
+    },
+
+    {
+        'crwebb85/luasnip-lsp-server.nvim',
+        dependencies = {
+            { 'L3MON4D3/LuaSnip' },
+        },
+        -- dev = true,
+        config = true,
+    },
+
+    {
         'williamboman/mason.nvim',
         lazy = true,
         event = 'VeryLazy',
